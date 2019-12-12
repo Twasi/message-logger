3183b1e44a8ebcbc06be3390d1cada5cb9f2c492
Lars Bärtschi <lars.baertschi@bluewin.ch>
2019-12-12 16:24:21 +0100

    switch from dependency to autoInstall hidden plugin

--- src/main/java/net/twasiplugin/dependency/messagelogger/MessageLoggerPlugin.java
+++ src/main/java/net/twasiplugin/dependency/messagelogger/MessageLoggerPlugin.java
@@ -1,9 +1,9 @@
 package net.twasiplugin.dependency.messagelogger;
 
-import net.twasi.core.plugin.TwasiDependency;
+import net.twasi.core.plugin.TwasiPlugin;
 import net.twasi.core.plugin.api.TwasiUserPlugin;
 
-public class MessageLoggerDependency extends TwasiDependency {
+public class MessageLoggerPlugin extends TwasiPlugin {
     @Override
     public Class<? extends TwasiUserPlugin> getUserPluginClass() {
         return MessageLogger.class;
